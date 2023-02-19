321a526b5c09e10aba91ecf2fbe0d6c84a121306
xsnakee <nikolay.extend@gmail.com>
2023-02-19 12:26:33 +0300

    refactored equatables. added converter tests.

--- src/converter/converter.go
+++ src/converter/converter.go
@@ -1,8 +1,33 @@
 package currency_converter
 
+import (
+	"errors"
+	"math"
+)
+
 type converter struct{}
 
-func (converter *converter) Convert(from currency, to currency, amount float64) (float64, error) {
-	// if from == nil || to == nil || math.from.rate || to.rate
-	return float64(amount) / float64(from.rate) * float64(to.rate), nil
+func (converter *converter) Convert(src currency, dest currency, amount float64) (float64, error) {
+
+	srcRateIsZero, _ := src.rate.Equals(0)
+	if srcRateIsZero {
+		return 0, errors.New("src Rate is empty")
+	}
+
+	destRateIsZero, _ := dest.rate.Equals(0)
+	if destRateIsZero {
+		return 0, errors.New("dest Rate is empty")
+	}
+
+	return float64(dest.rate) * float64(amount) / float64(src.rate), nil
+}
+
+const float64EquantityThreshold = 1e-9
+
+func (value currencyRate) Equals(otherValue currencyRate) (bool, error) {
+	return equals(float64(value), float64(otherValue))
+}
+
+func equals(value float64, otherValue float64) (bool, error) {
+	return math.Abs(value-otherValue) <= float64EquantityThreshold, nil
 }
